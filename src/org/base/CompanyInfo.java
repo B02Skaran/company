eb54d5f6577d7fdce20df552d5d95a39dd3b7c1f
B02Skaran <baskaran0202@gmail.com>
2022-03-05 18:10:18 +0530

    RET12345

---
+++ src/org/base/CompanyInfo.java
@@ -0,0 +1,41 @@
+package org.base;
+
+import java.util.*;
+
+public class CompanyInfo {
+	
+	private int empId;
+	private String name;
+	private long mobile;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
